ef7a473ae5ea603ab688f31dfee033d81a068948
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-11 21:46:31 -0400

    projects link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faProjectDiagram, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 // import LogoZgm from '../../assets/images/logo-color.svg'
 // import LogoZ from '../../assets/images/logo-no-background.svg'
@@ -22,6 +22,9 @@ const Navbar = () => (
         <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
             <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
+        <NavLink exact="true" activeclassname="active" className="project-link" to="/projects">
+            <FontAwesomeIcon icon={faProjectDiagram} color="#4d4d4e" />
+        </NavLink>
     </nav>
     <ul>
         <li>
@@ -30,7 +33,7 @@ const Navbar = () => (
             </a>
         </li>
         <li>
-            <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/zack-miller-813042186/'>
+            <a target="_blank" rel='noreferrer' href='https://github.com/ZmCoder2'>
                 <FontAwesomeIcon icon={faGithub} color="#4d4d4e" className="anchor-icon" />
             </a>
         </li>
