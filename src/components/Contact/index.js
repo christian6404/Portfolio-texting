a3c2b1a1d36f1e5914880d8ee5e89a3b5b75e089
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-12 23:56:11 -0400

    contact pathing with loader

---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import './index.scss'
+
+const Contact = () => {
+
+    return (
+        <>
+        <div className='container contact-page'></div>
+        <Loader type="cube-transition"/>
+        </>
+    )
+}
+
+export default Contact
